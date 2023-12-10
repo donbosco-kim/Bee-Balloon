7676ce4930c69f135a01b5ae30f19be95032e079
Donbosco Kim <leonbosco2@gmail.com>
2023-12-10 17:56:19 -0600

    add timer, pause-resume func.
    -Need to set timer to 4 mins as win condition.

---
+++ Assets/_Scripts/PauseMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    //public void Restart()
+    //{
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    //}
+}
--- Assets/_Scripts/SpawnPoint.cs
+++ Assets/_Scripts/SpawnPoint.cs
@@ -7,11 +7,14 @@ public class SpawnPoint: MonoBehaviour
 {
 
     [SerializeField] private GameObject PlayerPrefab;
+    [SerializeField] private Timer timer;
 
     public void OnStartClick()
     {
        GameObject GO = GameObject.Instantiate(PlayerPrefab);
         GO.transform.position = transform.position;
         gameObject.SetActive(false);
+        //Start the timer when the button is clicked
+        timer.StartTimer();
     }
 }
