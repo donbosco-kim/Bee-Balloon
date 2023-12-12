a25457184f0d6cee63004d610be1a39ffed55c2e
carter scranton <63666550+cgs2xw@users.noreply.github.com>
2023-12-11 20:00:31 -0600

    added balloon animation +bug fix

--- Assets/_Scripts/Balloon.cs
+++ Assets/_Scripts/Balloon.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Balloon : MonoBehaviour
 {
     public int value; //change the value in Balloon editor. This value represent how many points per balloon
 
+    Animator m_Animator;
+    float sleepTime = .5f;
+
     void Start()
     {
-        
+        m_Animator = gameObject.GetComponent<Animator>();
     }
     void Update()
     {
@@ -22,8 +26,19 @@ public class Balloon : MonoBehaviour
         {
             //Destroy balloon and increment score
             Camera.main.GetComponent<LevelController>().ballonPickUp(gameObject);
-            Destroy(gameObject);
+            m_Animator.SetTrigger("pop");
             ScoreCounter.instance.IncreaseCoins(value);
+            StartCoroutine(Destroytimer());
         }
     }
+
+
+
+    private IEnumerator Destroytimer()
+    {
+        yield return new WaitForSeconds(sleepTime);
+        Destroy(gameObject);
+    }
+
+
 }
